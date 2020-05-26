f69cff0eae11893b5adf5e72746f7c463a5d1444
proneko <62175517+proneko@users.noreply.github.com>
2020-05-26 12:01:06 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -53,43 +53,43 @@ window.addEventListener('DOMContentLoaded', function() {
       $('#bg2').css('opacity', '0.2');
     }
     if (dy > 1400) {
-      $('#bg2').css('opacity', '0.3');
-    }
-    if (dy > 1500) {
       $('#bg2').css('opacity', '0.4');
     }
-    if (dy > 1600) {
-      $('#bg2').css('opacity', '0.5');
-    }
-    if (dy > 1700) {
+    if (dy > 1500) {
       $('#bg2').css('opacity', '0.6');
     }
-    if (dy > 1800) {
-      $('#bg2').css('opacity', '0.7');
-    }
-    if (dy > 1900) {
+    if (dy > 1600) {
       $('#bg2').css('opacity', '0.8');
     }
+    if (dy > 1700) {
+      $('#bg2').css('opacity', '1');
+    }
+    if (dy > 1800) {
+      $('#bg2').css('opacity', '1');
+    }
+    if (dy > 1900) {
+      $('#bg2').css('opacity', '1');
+    }
     if (dy > 2000) {
-      $('#bg2').css('opacity', '0.9');
+      $('#bg2').css('opacity', '1');
     }
     if (dy > 2100) {
       $('#bg2').css('opacity', '1');
     }
     if (dy > 2200) {
-      $('#bg2').css('opacity', '0.9');
+      $('#bg2').css('opacity', '1');
     }
     if (dy > 2300) {
-      $('#bg2').css('opacity', '0.8');
+      $('#bg2').css('opacity', '1');
     }
     if (dy > 2400) {
-      $('#bg2').css('opacity', '0.7');
+      $('#bg2').css('opacity', '1');
     }
     if (dy > 2500) {
-      $('#bg2').css('opacity', '0.6');
+      $('#bg2').css('opacity', '0.8');
     }
     if (dy > 2600) {
-      $('#bg2').css('opacity', '0.5');
+      $('#bg2').css('opacity', '0.6');
     }
     if (dy > 2700) {
       $('#bg2').css('opacity', '0.4');
@@ -125,37 +125,37 @@ window.addEventListener('DOMContentLoaded', function() {
       $('#bg3').css('opacity', '0.4');
     }
     if (dy > 3700) {
-      $('#bg3').css('opacity', '0.5');
-    }
-    if (dy > 3800) {
       $('#bg3').css('opacity', '0.6');
     }
-    if (dy > 3900) {
-      $('#bg3').css('opacity', '0.7');
-    }
-    if (dy > 4000) {
+    if (dy > 3800) {
       $('#bg3').css('opacity', '0.8');
     }
+    if (dy > 3900) {
+      $('#bg3').css('opacity', '1');
+    }
+    if (dy > 4000) {
+      $('#bg3').css('opacity', '1');
+    }
     if (dy > 4100) {
-      $('#bg3').css('opacity', '0.9');
+      $('#bg3').css('opacity', '1');
     }
     if (dy > 4200) {
       $('#bg3').css('opacity', '1');
     }
     if (dy > 4300) {
-      $('#bg3').css('opacity', '0.9');
+      $('#bg3').css('opacity', '1');
     }
     if (dy > 4400) {
-      $('#bg3').css('opacity', '0.8');
+      $('#bg3').css('opacity', '1');
     }
     if (dy > 4500) {
-      $('#bg3').css('opacity', '0.7');
+      $('#bg3').css('opacity', '1');
     }
     if (dy > 4600) {
-      $('#bg3').css('opacity', '0.6');
+      $('#bg3').css('opacity', '0.8');
     }
     if (dy > 4700) {
-      $('#bg3').css('opacity', '0.5');
+      $('#bg3').css('opacity', '0.6');
     }
     if (dy > 4800) {
       $('#bg3').css('opacity', '0.4');
@@ -193,37 +193,37 @@ window.addEventListener('DOMContentLoaded', function() {
       $('#bg4').css('opacity', '0.4');
     }
     if (dy > 5800) {
-      $('#bg4').css('opacity', '0.5');
-    }
-    if (dy > 5900) {
       $('#bg4').css('opacity', '0.6');
     }
-    if (dy > 6000) {
-      $('#bg4').css('opacity', '0.7');
-    }
-    if (dy > 6100) {
+    if (dy > 5900) {
       $('#bg4').css('opacity', '0.8');
     }
+    if (dy > 6000) {
+      $('#bg4').css('opacity', '1');
+    }
+    if (dy > 6100) {
+      $('#bg4').css('opacity', '1');
+    }
     if (dy > 6200) {
-      $('#bg4').css('opacity', '0.9');
+      $('#bg4').css('opacity', '1');
     }
     if (dy > 6300) {
       $('#bg4').css('opacity', '1');
     }
     if (dy > 6400) {
-      $('#bg4').css('opacity', '0.9');
+      $('#bg4').css('opacity', '1');
     }
     if (dy > 6500) {
-      $('#bg4').css('opacity', '0.8');
+      $('#bg4').css('opacity', '1');
     }
     if (dy > 6600) {
-      $('#bg4').css('opacity', '0.7');
+      $('#bg4').css('opacity', '1');
     }
     if (dy > 6700) {
-      $('#bg4').css('opacity', '0.6');
+      $('#bg4').css('opacity', '0.8');
     }
     if (dy > 6800) {
-      $('#bg4').css('opacity', '0.5');
+      $('#bg4').css('opacity', '0.6');
     }
     if (dy > 6900) {
       $('#bg4').css('opacity', '0.4');
@@ -245,49 +245,60 @@ window.addEventListener('DOMContentLoaded', function() {
       $('#bg5').css('opacity', '0');
       $('#bg0').css('opacity', '1');
       $('#bg5').css('z-index', '99');
+      $('#yazi').css('opacity', '1');
     }else {
       $('#bg5').css('z-index', '-99');
       $('#bg5').css('opacity', '0');
     }
     if (dy > 7500) {
       $('#bg5').css('opacity', '0.1');
-      $('#bg0').css('opacity', '0.9');
+      $('#bg0').css('opacity', '1');
+      $('#yazi').css('opacity', '1');
     }
     if (dy > 7600) {
       $('#bg5').css('opacity', '0.2');
-      $('#bg0').css('opacity', '0.8');
+      $('#bg0').css('opacity', '1');
+      $('#yazi').css('opacity', '1');
     }
     if (dy > 7700) {
       $('#bg5').css('opacity', '0.3');
-      $('#bg0').css('opacity', '0.7');
+      $('#bg0').css('opacity', '1');
+      $('#yazi').css('opacity', '1');
     }
     if (dy > 7800) {
       $('#bg5').css('opacity', '0.4');
-      $('#bg0').css('opacity', '0.6');
+      $('#bg0').css('opacity', '0.8');
+      $('#yazi').css('opacity', '0.8');
     }
     if (dy > 7900) {
-      $('#bg5').css('opacity', '0.5');
-      $('#bg0').css('opacity', '0.5');
+      $('#bg5').css('opacity', '0.6');
+      $('#bg0').css('opacity', '0.6');
+      $('#yazi').css('opacity', '0.6');
     }
     if (dy > 8000) {
-      $('#bg5').css('opacity', '0.6');
+      $('#bg5').css('opacity', '0.8');
       $('#bg0').css('opacity', '0.4');
+      $('#yazi').css('opacity', '0.4');
     }
     if (dy > 8100) {
-      $('#bg5').css('opacity', '0.7');
+      $('#bg5').css('opacity', '1');
       $('#bg0').css('opacity', '0.3');
+      $('#yazi').css('opacity', '0.3');
     }
     if (dy > 8200) {
-      $('#bg5').css('opacity', '0.8');
+      $('#bg5').css('opacity', '1');
       $('#bg0').css('opacity', '0.2');
+      $('#yazi').css('opacity', '0.2');
     }
     if (dy > 8300) {
-      $('#bg5').css('opacity', '0.9');
+      $('#bg5').css('opacity', '1');
       $('#bg0').css('opacity', '0.1');
+      $('#yazi').css('opacity', '0.1');
     }
     if (dy > 8400) {
       $('#bg5').css('opacity', '1');
       $('#bg0').css('opacity', '0');
+      $('#yazi').css('opacity', '0');
     }
 
   });
